7f4ca6190a4ddb06395f698c619b2b4513aeceb0
Brendan Roy <br3ndanr@gmail.com>
2020-04-06 15:14:21 +1000

    update action

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const {core} = require('@actions/core');
+const {github} = require('@actions/github');
 
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`The context payload: ${payload}`);
+  const ev = JSON.stringify(github.event, undefined, 2)
+  console.log(`The event payload: ${ev}`);
 } catch (error) {
   core.setFailed(error.message);
 }
