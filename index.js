bd4acab36022453dceb7d0cded294079606ab9a2
Brendan Roy <br3ndanr@gmail.com>
2020-04-06 16:38:50 +1000

    update action

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {core} = require('@actions/core');
-const {github} = require('@actions/github');
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
