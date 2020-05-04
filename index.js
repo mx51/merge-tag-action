4ee99bc088584030643fb02bbaa820bc66fcc4cd
Brendan Roy <br3ndanr@gmail.com>
2020-05-04 16:58:12 +1000

    update action

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const github = require('@actions/github');
 
 async function run() {
   try {
-    const client = new github.GitHub(github.token);
-    console.log("tok: "+ core.getInput('repo-token'));
-    console.log("tok2: "+ github.token);
+    const client = new github.GitHub(core.getInput('repo-token'));
 
     const {
       payload: {pull_request: pullRequest, repository},
