2d58d3ab8f342125c87523d358fa6a98945d64a2
Brendan Roy <br3ndanr@gmail.com>
2020-05-04 14:34:46 +1000

    update index.js

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  const client = new github.Github(core.getInput('githubToken'));
+
+  const {
+    payload: { pull_request: pullRequest, repository },
+    sha: commitSha,
+  } = github.context;
+  console.log(`sha: ${commitSha}`)
+
+  const { full_name: repoFullName } = repository;
+  const [owner, repo] = repoFullName.split("/");
+  const pullNumber = pullRequest.number;
+  const prRef = {
+    owner,
+    repo,
+    pull_number: pullNumber,
+  }
+  const { data: comments } = await client.pulls.listComments(prRef);
+
+  console.log(`comments: ${comments}`)
+
   // Get the JSON webhook payload for the event that triggered the workflow
   console.log(`merged: ${github.context.payload.pull_request.merged}`)
   console.log(`action: ${github.context.payload.action}`)
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The context payload: ${payload}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
