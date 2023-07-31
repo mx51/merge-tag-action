cacc5acba46ecb1c846b26ec3586cb538a950cb5
Mike Gouline <1960272+gouline@users.noreply.github.com>
2023-07-31 16:53:14 +1000

    Add action outputs with version and tag status (#32)

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ async function run() {
   const changeType = await getChangeTypeForContext(client);
   if (changeType !== "" && changeType !== "notag") {
     const nextVersion = await getNewVersionTag(changeType);
-    core.setOutput('NEXT_VERSION', `${nextVersion}`);
+    core.setOutput('version', `${nextVersion}`);
     // if just merged, tag and release
     if (github.context.payload.action === "closed" && github.context.payload.pull_request.merged === true) {
       return createAnnotatedTag(client, nextVersion).then(() => {
+        core.setOutput('tagged', true);
         return createPRCommentOnce(client, `Merged and tagged as \`${nextVersion}\`.`)
       });
     }
